25ddb3273f8644605430f061dc341738b59c71a6
oscar <oscar.carrillo758@gmail.com>
2025-03-23 02:06:06 -0700

    fifth commit

--- db.js
+++ db.js
@@ -6,22 +6,23 @@ const { getDatabaseUri } = require("./config");
 
 let db;
 
-// Use SSL connection for production environment
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  // Use non-SSL connection for development and test environments
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
+// Use SSL connection for all environments
+const connectionString = getDatabaseUri();
+const sslConfig = {
+  rejectUnauthorized: false
+};
+
+console.log("Connecting to database with connection string:", connectionString);
+console.log("SSL configuration:", sslConfig);
+
+db = new Client({
+  connectionString: connectionString,
+  ssl: sslConfig
+});
 
 // Connect to the database
-db.connect();
+db.connect()
+  .then(() => console.log("Connected to the database successfully"))
+  .catch(err => console.error("Error connecting to the database:", err));
 
 module.exports = db;
